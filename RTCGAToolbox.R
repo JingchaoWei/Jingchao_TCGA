d90c3e89f5b5e3997d6aee040c1ce214e7c1e779
JingchaoWei <40045699+JingchaoWei@users.noreply.github.com>
2018-11-07 16:15:51 -0800

    3

--- RTCGAToolbox.R
+++ RTCGAToolbox.R
@@ -5,8 +5,37 @@ stddate <- getFirehoseRunningDates()
 stddate
 gisticDate <- getFirehoseAnalyzeDates()
 gisticDate
-PRAD_data <- getFirehoseData(dataset="PRAD", runDate="20151101",forceDownload = F,
-                             clinical=TRUE, RNASeq2GeneNorm = T,Mutation = T)
+brcaData <- getFirehoseData (dataset="BRCA", runDate="20140416",
+                             gistic2Date="20140115", clinic=TRUE, RNAseqGene=TRUE, mRNAArray=TRUE,
+                             Mutation=TRUE)
+
 data(RTCGASample)
 RTCGASample
 getData(RTCGASample, "RNASeqGene")
+par(mfrow=c(2,2))
+diffGeneExprs <- getDiffExpressedGenes(dataObject=RTCGASample, DrawPlots=TRUE,
+                                       adj.method="BH", adj.pval=0.05, raw.pval=0.05, logFC=2, hmTopUpN=10,
+                                       hmTopDownN=100)
+diffGeneExprs
+showResults(diffGeneExprs[[1]])
+toptableOut <- showResults(diffGeneExprs[[1]])
+corrGECN <- getCNGECorrelation(dataObject=RTCGASample, adj.method="BH",
+                               adj.pval=0.9, raw.pval=0.05)
+corrGECN
+showResults(corrGECN[[1]])
+corRes <- showResults(corrGECN[[1]])
+mutFrq <- getMutationRate(dataObject=RTCGASample)
+head(mutFrq[order(mutFrq[, 2], decreasing=TRUE), ])
+clinicData <- getData(RTCGASample,"clinical")
+head(clinicData)
+clinicData <- clinicData[, 3:5]
+clinicData[is.na(clinicData[, 3]), 3] <- clinicData[is.na(clinicData[, 3]), 2]
+survData <- data.frame(Samples=rownames(clinicData),
+                       Time=as.numeric(clinicData[, 3]), Censor=as.numeric(clinicData[, 1]))
+getSurvival(dataObject=RTCGASample, geneSymbols=c("FCGBP"), sampleTimeCensor=survData)
+
+
+
+
+
+
